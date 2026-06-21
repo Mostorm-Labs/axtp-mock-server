34128cd56124e3f0af203ea618e7a478e40c614e
Evan <136231429+luckievan@users.noreply.github.com>
2026-06-21 10:45:11 +0800

    chore: upgrade AXTP Spec to spec/v0.8.9 (#14)

--- generated/cpp-mock-server/include/axtp_generated_version.hpp
+++ generated/cpp-mock-server/include/axtp_generated_version.hpp
@@ -4,12 +4,12 @@
 namespace axtp::generated {
 
 inline constexpr const char* kRuntimeName = "axtp-mock-server";
-inline constexpr const char* kRuntimeVersion = "0.8.8.0";
-inline constexpr const char* kSpecVersion = "0.8.8";
-inline constexpr const char* kSpecTag = "spec/v0.8.8";
-inline constexpr const char* kSpecCommit = "1d2e948b1756ac8cc4d3cdcdeb9626e41151e909";
+inline constexpr const char* kRuntimeVersion = "0.8.9.0";
+inline constexpr const char* kSpecVersion = "0.8.9";
+inline constexpr const char* kSpecTag = "spec/v0.8.9";
+inline constexpr const char* kSpecCommit = "4758399ed08012309a8d11097b2c9569ed3df50c";
 inline constexpr const char* kGeneratorName = "@axtp/mock-server-generator";
 inline constexpr const char* kGeneratorVersion = "1.0.0";
-inline constexpr const char* kGeneratedAt = "2026-06-21T00:58:07Z";
+inline constexpr const char* kGeneratedAt = "2026-06-21T02:44:52Z";
 
 }  // namespace axtp::generated
