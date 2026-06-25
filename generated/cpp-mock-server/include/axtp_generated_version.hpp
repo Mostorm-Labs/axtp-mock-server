52571345c7614720ea8f4a5a39ceb66563f50b86
luckievan <136231429+luckievan@users.noreply.github.com>
2026-06-25 02:34:26 +0000

    chore: upgrade AXTP Spec to spec/v0.11.0

--- generated/cpp-mock-server/include/axtp_generated_version.hpp
+++ generated/cpp-mock-server/include/axtp_generated_version.hpp
@@ -4,12 +4,12 @@
 namespace axtp::generated {
 
 inline constexpr const char* kRuntimeName = "axtp-mock-server";
-inline constexpr const char* kRuntimeVersion = "0.10.0.0";
-inline constexpr const char* kSpecVersion = "0.10.0";
-inline constexpr const char* kSpecTag = "spec/v0.10.0";
-inline constexpr const char* kSpecCommit = "52cdcd6dc186a1638a7942d72cf5450334b476af";
+inline constexpr const char* kRuntimeVersion = "0.11.0.0";
+inline constexpr const char* kSpecVersion = "0.11.0";
+inline constexpr const char* kSpecTag = "spec/v0.11.0";
+inline constexpr const char* kSpecCommit = "6e9594a268e840d04aa75b1de065f7d4c2debcc1";
 inline constexpr const char* kGeneratorName = "@axtp/mock-server-generator";
 inline constexpr const char* kGeneratorVersion = "1.0.0";
-inline constexpr const char* kGeneratedAt = "2026-06-23T13:56:09Z";
+inline constexpr const char* kGeneratedAt = "2026-06-25T02:34:12Z";
 
 }  // namespace axtp::generated
